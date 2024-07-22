445104a7067063e6464d3046a67916098a475201
Althaf717 <muhammedalthaf717@gmail.com>
2024-07-22 19:36:42 +0530

    connected mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,18 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGODB_URL).then(()=>{
+    console.log("mongodb connected");
+})
+.catch((err)=>{
+    console.log('mongodb error:'+err);
+}) 
 
 const app = express();
 
 app.listen(3000,()=>{
     console.log('Server listening on port 3000!!!');
-})
+}) 
