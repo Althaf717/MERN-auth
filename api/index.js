187029f5125140b6172c51a6774f15eb247dbe0a
Althaf717 <muhammedalthaf717@gmail.com>
2024-07-24 11:12:35 +0530

    some changes in this file

--- api/index.js
+++ api/index.js
@@ -16,12 +16,12 @@ mongoose.connect(process.env.MONGODB_URL).then(()=>{
 const app = express();
 app.use(express.json())
 
-app.listen(3000,()=>{
-    console.log('Server listening on port 3000!!!');
+app.listen(5000,()=>{
+    console.log('Server listening on port 5000!!!');
 }) 
 
-app.use('/user',userRoutes)
-app.use('/auth',authRoutes)
+app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500
     const message = err.message || "Internal Server Error"
