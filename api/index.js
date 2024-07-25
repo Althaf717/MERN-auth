5d0cd9867f262abd50da15a4f27f9ca058e472c0
Althaf717 <muhammedalthaf717@gmail.com>
2024-07-25 10:33:43 +0530

    some updation and modification

--- api/index.js
+++ api/index.js
@@ -2,9 +2,16 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
-import authRoutes from './routes/auth.route.js'
+import cors from 'cors'
 
 dotenv.config()
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true, // If your frontend sends credentials (like cookies)
+}));
 
 mongoose.connect(process.env.MONGODB_URL).then(()=>{
     console.log("mongodb connected");
@@ -13,15 +20,14 @@ mongoose.connect(process.env.MONGODB_URL).then(()=>{
     console.log('mongodb error:'+err);
 }) 
 
-const app = express();
+
 app.use(express.json())
 
-app.listen(5000,()=>{
-    console.log('Server listening on port 5000!!!');
+app.listen(8000,()=>{
+    console.log('Server listening on port 8000!!!');
 }) 
 
 app.use('/api/user',userRoutes)
-app.use('/api/auth',authRoutes)
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500
     const message = err.message || "Internal Server Error"
--- api/routes/auth.route.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import authController from '../controllers/auth.controller.js'
-const router = express.Router()
-
-router.post('/signup',authController.signup)
-
-export default router
