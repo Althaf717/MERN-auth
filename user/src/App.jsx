346e4c1dd6c4674929fbf9d74153055f23226b47
Althaf717 <muhammedalthaf717@gmail.com>
2024-08-08 10:42:53 +0530

    solve the private component issue

--- user/src/App.jsx
+++ user/src/App.jsx
@@ -1,34 +1,31 @@
-import React from 'react'
-import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Profile from './pages/Profile'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import Header from './components/Header'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import PrivateRoute from './components/PrivateRoute'
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
     <Router>
-      <Header/>
-      <ToastContainer/>
+      <Header />
+      <ToastContainer />
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/about" element={<About/>} />
-        <Route path="/profile" element={<Profile/>} />
-        <Route path="/sign-in" element={<SignIn/>} />
-        <Route path="/sign-up" element={<SignUp/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route element={<PrivateRoute />}>
-        <Route path='/profile' element={<Profile/>} />
+          <Route path="/profile" element={<Profile />} />
         </Route>
-        
       </Routes>
-      
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
